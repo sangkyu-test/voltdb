01d2a01e5864518ef59149bfac33c9cacf4f18cb
sangkyu-test <60460309+sangkyu-test@users.noreply.github.com>
2020-02-27 11:41:17 +0900

    Update voltdb-k8s-utils.sh

--- voltdb-k8s-utils.sh
+++ voltdb-k8s-utils.sh
@@ -68,33 +68,22 @@ build_configmap_from_files() {
     rm -f $ORDERFILE
 }
 
-
-kubectl_ifexists() {
-    RC=0
-    if kubectl get "$2"/"$3" &>/dev/null; then
-        kubectl $@
-        RC=$?
-    fi
-    return $RC
-}
-
-
 make_configmaps() {
 
     # make classes configmap
     # packages voltdb procedures/udf classes jar files
-    MAPNAME=${CLUSTER_NAME}-init-classes
+    MAPNAME=voltdb-init-classes
     build_configmap_from_files $MAPNAME "${CLASSES_FILES}"
 
     # make schema configmap
     # packages voltdb ddl files
-    MAPNAME=${CLUSTER_NAME}-init-schema
+    MAPNAME=voltdb-init-schema
     kubectl_ifexists delete configmap "$MAPNAME"
     build_configmap_from_files $MAPNAME "${SCHEMA_FILES}"
 
     # make init configmap
     # packages: deployment.xml, license.xml, log4j properties
-    MAPNAME=${CLUSTER_NAME}-init-configmap
+    MAPNAME=voltdb-init-configmap
     kubectl_ifexists delete configmap "$MAPNAME"
     CONFIG_MAP_ARGS=""
     [[ -n ${DEPLOYMENT_FILE} ]] && CONFIG_MAP_ARGS+=" --from-file=deployment=${DEPLOYMENT_FILE}"
@@ -105,7 +94,7 @@ make_configmaps() {
 
     # make runtime environment settings configmap
     # packages voltdb runtime settings NODECOUNT, VOLTDB_OPTS, etc.
-    MAPNAME=${CLUSTER_NAME}-run-env
+    MAPNAME=voltdb-run-env
     kubectl_ifexists delete configmap "$MAPNAME"
 
     PROP_FILE="$(mktemp)"
@@ -121,165 +110,3 @@ make_configmaps() {
 
     rm -f ${PROP_FILE}
 }
-
-
-build_image() {
-
-    # Requires a voltdb kit be installed (and we should be running from it)
-
-    # For voltdb bundles and extensions, manually copy your assets to the corresponding directories
-    # prior to creating the image
-
-    if [[ ! -d ../../bin ]]; then
-        echo "ERROR VoltDB tree structure error, VoltDB binaries not found"
-        exit -1
-    fi
-
-    # Build Tag Deploy the image
-    # nb. the docker build environment will encompass the VoltDB kit
-    #     Some content that is not normally required for production --doc and examples-- are removed, see Dockerfile
-
-    pushd ../.. > /dev/null
-
-    docker image build -t ${IMAGE_TAG:-$CLUSTER_NAME} \
-                       --build-arg VOLTDB_DIST_NAME=$(basename $(pwd)) \
-                       -f tools/kubernetes/docker/Dockerfile \
-                       "$PWD"
-
-    # tag and push as appropriate
-    [[ -n ${IMAGE_TAG} ]] && docker tag ${IMAGE_TAG} ${REP}/${IMAGE_TAG}
-    [[ -n ${REP} ]] && docker push ${REP}/${IMAGE_TAG}
-
-    popd > /dev/null
-
-    }
-
-
-start_voltdb() {
-    if kubectl_ifexists scale statefulset ${CLUSTER_NAME} --replicas=$NODECOUNT; then
-        kubectl create -f ${CLUSTER_NAME}.yaml
-    fi
-    }
-
-
-stop_voltdb() {
-    # Quiesce the cluster, put it into admin mode, then scale it down to zero replicas (nodes)
-    local PODS=$(kubectl get pods --no-headers=true 2>/dev/null | grep "$CLUSTER_NAME" )
-    RUNNING_POD=$(echo "$PODS" | grep Running | head -1 | cut -d\  -f1)
-    if [[ -z $RUNNING_POD ]]; then
-        echo "ERROR: no running pods for cluster '$CLUSTER_NAME' were found"
-        echo "$PODS"
-        exit 1
-    fi
-    if ! kubectl exec ${RUNNING_POD} -- voltadmin pause --wait; then
-        echo "ERROR: unable to pause/drain cluster '$CLUSTER_NAME', is the cluster operational?"
-        exit 1
-    fi
-    kubectl scale statefulset ${CLUSTER_NAME} --replicas=0
-    }
-
-
-force_voltdb() {
-
-    # !!! Your voltdb statefulset and its pod(s) will be forced to terminate ungracefully
-
-    for P in $(kubectl get pods --no-headers=true 2>/dev/null| egrep "^$CLUSTER_NAME" | cut -d\  -f1)
-    do
-        kubectl delete pods "$P" --grace-period=0 --force
-    done
-
-    kubectl_ifexists delete statefulset $CLUSTER_NAME --grace-period=0 --force
-    kubectl_ifexists delete service $CLUSTER_NAME
-}
-
-
-purge_persistent_claims() {
-
-    # !!! You will loose your related persistent volumes (all your data), with no possibility of recovery
-
-    for P in $(kubectl get pvc 2>/dev/null | egrep "$CLUSTER_NAME" | cut -d\  -f1)
-    do
-        kubectl delete pvc $P
-    done
-}
-
-usage() {
-    echo "Usage: $0 parameter-file options ..."
-    echo "      -B --build-voltdb-image         Builds a docker image of voltdb and pushes it to your repo"
-    echo "      -M --install-configmap          Run kubectl create configmap for your config"
-    echo "      -C --configure-voltdb           Customize the voltdb-manifest with provided parameters"
-    echo "      -S --start-voltdb               Run kubectl create <voltdb-manifest> or kubectl .. --replicas="
-    echo "      -P --stop-voltdb                Run voltadmin pause --wait; kubectl scale <voltdb-manifest> replicas=0"
-    echo "      -P --restart-voltdb             Shorthand for -P -S"
-    echo "      -D --purge-persistent-volumes   Run kubectl delete on clusters persistent volumes"
-    echo "      -F --force-voltdb-down          Run kubectl delete cluster statefulset and running pods"
-    exit 1
-}
-
-
-# MAIN
-
-# source the template settings
-if [[ ! -e "$1" ]]; then
-    echo "ERROR parameter file not specified, customize the template with your settings and database assets"
-    usage
-fi
-
-# source our config file parameters
-CFG_FILE=$1
-source ${CFG_FILE}
-
-if [[ -z $CLUSTER_NAME ]]; then
-    echo "ERROR: CLUSTER_NAME unspecified"
-    usage
-fi
-
-shift 1
-
-# use Cluster name as default image name
-: ${IMAGE_TAG:=${CLUSTER_NAME}}
-
-if [[ $# -eq 0 ]]; then
-    echo "ERROR option(s) missing"
-    usage
-fi
-
-for cmd in "$@"
-do
-    case $cmd in
-
-        -B|--build-voltdb-image)
-                                    build_image
-        ;;
-        -M|--install-configmap)
-                                    make_configmaps
-        ;;
-        -C|--configure-voltdb)
-                                    make_statefulset
-        ;;
-        -S|--start-voltdb)
-                                    start_voltdb
-        ;;
-        -P|--stop-voltdb)
-                                    stop_voltdb
-        ;;
-        -D|--purge-persistent-claims)
-                                    purge_persistent_claims
-        ;;
-        -F|--force-voltdb-set)
-                                    force_voltdb
-        ;;
-        -R|--restart_voltdb)
-                                    stop_voltdb
-                                    start_voltdb
-        ;;
-        -h|--help)
-                                    usage
-        ;;
-        *)
-                                    echo "ERROR unrecognized option"
-                                    usage
-                                    exit 1
-        ;;
-    esac
-done
